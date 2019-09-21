32aa1fc3f25278b3d6fa30e99f20f0863f094c55
wint0074 <wint0074@stthomas.edu>
2019-09-20 20:13:54 -0500

    first commit

---
+++ main.tf
@@ -0,0 +1,27 @@
+provider "google" {
+  project = "sixth-arbor-252201"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network       = "default"
+    access_config {
+    }
+  }
+}
+resource "google_compute_network" "vpc_network" {
+  name                    = "terraform-network"
+  auto_create_subnetworks = "true"
+}
+
